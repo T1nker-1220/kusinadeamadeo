51f377d311de3a8c18fb0fc688e210057bd20c89
T1nker-1220 <marquezjohnnathanieljade@gmail.com>
2025-06-15 20:11:21 +0800

    update

--- src/app/order-success/page.tsx
+++ src/app/order-success/page.tsx
@@ -6,7 +6,7 @@ export default function OrderSuccessPage() {
       <h1 className="text-4xl font-bold text-green-600">Order Received!</h1>
       <p className="mt-4 text-lg">Thank you for your purchase. We are now preparing your order.</p>
       <p className="mt-2">You can wait for your order at our store. Please be ready to present your name.</p>
-      <Link href="/" className="mt-8 inline-block bg-yellow-500 text-black font-bold py-2 px-6 rounded-lg">
+      <Link href="/menu" className="mt-8 inline-block bg-yellow-500 text-black font-bold py-2 px-6 rounded-lg">
         Back to Menu
       </Link>
     </div>
