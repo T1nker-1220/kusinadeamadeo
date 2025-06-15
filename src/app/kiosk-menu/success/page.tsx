4dcfa4c035d902f4ee2af33d88aa475c49d006fa
T1nker-1220 <marquezjohnnathanieljade@gmail.com>
2025-06-15 23:08:58 +0800

    Remove KioskSuccessClient component and update import path in KioskSuccessPage for better organization.

--- src/app/kiosk-menu/success/page.tsx
+++ src/app/kiosk-menu/success/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import KioskSuccessClient from './KioskSuccessClient';
+import KioskSuccessClient from '../../../components/customers/KioskSuccessClient';
 
 function LoadingFallback() {
   return (
